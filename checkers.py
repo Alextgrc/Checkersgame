8d35644a5cda1239307416d58399b67520a36ee5
Alextgrc <32960191+Alextgrc@users.noreply.github.com>
2017-11-09 14:49:02 +0000

    Checkers with AI and 2 player mode
    Found and solved a bug in the common function undoRedo for the both players (2 player mode).

--- checkers.py
+++ checkers.py
@@ -22,6 +22,8 @@ board_size=8
 y=0
 playeR= 1
 playerB=2
+playerKR=3
+playerKB=4
 turn=0
 #Converts numbers from the board into characters
 def intToText(num):
@@ -63,70 +65,59 @@ def twoPlayerMode():
 
 	if (turn==1):
 		validateB()
-def undoRedo():
-	undoBoard=input ('\n''Type undo if you would like to retake the turn. Otherwise press enter''\n')
-	if(undoBoard=='undo' or undoBoard=='Undo'):
-		board=copyBoard[:]
-		board=copy.deepcopy(copyBoard)
-		printGrid()
-		
-		redoBoard=input('\n''Type redo if you would like to retake the turn. Otherwise press enter''\n')
-		if(redoBoard=='redo' or redoBoard=='Redo'):
-			board=board2[:]
-			board=copy.deepcopy(board2)
-			printGrid()
-			turn=1
-		else:
-			turn=0
 #Checks the coordinates given are valid
 def validateR():
-		global piece, pieceX, pieceY, move, moveX, moveY, board,turn
-		piece = input('\n' 'It is player Rs turn. Choose your piece to move or EXIT to quit:' '\n')
-		if ( piece == 'help'):
-		    print('Type the coordinates (originating from the top left) of the box you want to put a cross into in the format \'y,x\' (e.g. 3,2) or EXIT to quit''\n')
-		if (piece == "EXIT" or piece=='exit'):
-			sys.exit()
-		#Checks if the input is on the correct format
-		if (len(piece) == 3):
-			splitcord= piece.split(',')
-			pieceX=int(splitcord[0])
-			pieceY=int(splitcord[1])
-			if(pieceX <8 and pieceX >=0 and pieceY <8 and pieceY >=0 ):	
-				#Checks if a players piece is there to be moved
-				if(board[pieceY][pieceX]== playeR):
-					print('\n' 'Right piece!' '\n')
-					#Checks if there's an enemy's piece there to be eaten and handles the exception for columns 0 and 7
-					copyBoard= board[:]
-					copyBoard= copy.deepcopy(board)
-					board2=board[:]
-					possibleMovesR()
-					undoRedo()	
-				else:
-					print('Cannot move this piece. Try again :)')
+	#This variables are global so they can be used in other definitions
+	global piece, pieceX, pieceY, move, moveX, moveY, board,turn, board2, copyBoard
+	piece = input('\n' 'It is player Rs turn. Choose your piece to move or EXIT to quit:' '\n')
+	if ( piece == 'help'):
+	    print('Type the coordinates (originating from the top left) of the box you want to put a cross into in the format \'y,x\' (e.g. 3,2) or EXIT to quit''\n')
+	if (piece == "EXIT" or piece=='exit'):
+		sys.exit()
+	#Checks if the input is on the correct format and splits it into two X and Y variables
+	if (len(piece) == 3):
+		splitcord= piece.split(',')
+		pieceX=int(splitcord[0])
+		pieceY=int(splitcord[1])
+		if(pieceX <8 and pieceX >=0 and pieceY <8 and pieceY >=0 ):	
+			#Checks if the chosen piece belongs to the user
+			if(board[pieceY][pieceX]== playeR):
+				print('\n' 'Right piece!' '\n')
+				#Stores board before and after the user moves the piece: used for Undo and Redo action
+				copyBoard= board[:]
+				copyBoard= copy.deepcopy(board)
+				board2=board[:]
+				possibleMovesR()
+				undoRedo()	
+			else:
+				print('Cannot move this piece. Try again :)')
 
-		else:
-			print('Invalid input. Type \'help\' if you\'re stuck')
+	else:
+		print('Invalid input. Type \'help\' if you\'re stuck')
 def possibleMovesR():
-	global newPosX, newPosY
+	global newPosX, newPosY,turn
+	#Handles exception incase values are out of range
 	try:
+		#Checks if the chosen piece has a enemy's piece nearby
 		if((board[pieceY+1][pieceX-1]==playerB or board[pieceY+1][pieceX+1]==playerB) and(board[pieceY+2][pieceX+2]==0 or board[pieceY+2][pieceX-2]==0)):
 			eat=input('\n' 'Available piece to be eaten.Press Y to eat or N not to' '\n')					
-			#Moves the piece chosen over the enemy's piece and eats it
+			#If yes is chosen, the eating action will be processed automatically
 			if(eat=='Y' or eat=='y'):
 				if(board[pieceY+1][pieceX-1]==playerB):
+					#Updates board
 					board[pieceY+2][pieceX-2]=board[pieceY][pieceX]
 					board[pieceY][pieceX]=0
 					board[pieceY+1][pieceX-1]=0
+					#Stores piece's new coordinates
 					newPosY=pieceY+2
 					newPosX=pieceX-2
-					
 					printGrid()
 					print('\n' 'CONGRATS! You ate an enemy piece!' '\n')
-					global turn
+					#Checks if there's another immediate piece to be eaten and executes it 
 					doubleJumpR()
 					printGrid()
-					turn=1
-					
+					#Changes turn to the other player
+					turn=1		
 				elif(board[pieceY+1][pieceX+1]==playerB):
 					board[pieceY+2][pieceX+2]=board[pieceY][pieceX]
 					board[pieceY][pieceX]=0
@@ -135,22 +126,22 @@ def possibleMovesR():
 					newPosX=pieceX+2
 					printGrid()
 					print('\n' 'CONGRATS! You ate an enemy piece!' '\n')
-					print(newPosX, newPosY)
 					doubleJumpR()
 					printGrid()
-					turn=1
-					
-			#Asks where do you want to move it instead
+					turn=1			
+			#For strategic reasons user does not want to eat the piece it will offer to move it somewhere else
 			if(eat=='N' or eat=='n'):
 				move=input('\n' 'Where do you want it instead?' '\n')
-				#Checks it's the right input format
+				#Checks the input is on the correct format
 				if (len(move) == 3):
 					splitcord= move.split(',')
 					moveX=int(splitcord[0])
 					moveY=int(splitcord[1])
-					if(board[moveY][moveX]!=playeR):
+					#Checks there isn't another piece and that values are on the range
+					if(board[moveY][moveX]!=0):
 						if(moveX >=0 and moveX <8 and moveY >=0 and moveY <8):
 							if(moveY==pieceY+1 and (moveX== pieceX+1 or moveX== pieceX-1)):
+								#Updates board
 								board[moveY][moveX]=board[pieceY][pieceX]
 								board[pieceY][pieceX]=0
 								printGrid()
@@ -160,14 +151,17 @@ def possibleMovesR():
 
 					else:
 						print('A piece is already there!')
+			else:
+				possibleMovesR()
 		elif((board[pieceY+1][pieceX-1]==playerB or board[pieceY+1][pieceX+1]==playerB) and(board[pieceY+2][pieceX+2]!=0 or board[pieceY+2][pieceX-2]!=0)):
 			print('...but there is not legal moves available. Try again :D')
 	except:
 		pass
 	try:	
+		#If there is no enemys pieces close to be eaten offers you to move said piece
 		if((board[pieceY+1][pieceX-1]==0 or board[pieceY+1][pieceX+1]==0)and turn!=1): 
 			move=input('Where do you want it instead?' '\n')
-
+			#Checks format, range, and correct piece chosen fullfil the game conditions
 			if (len(move) == 3):
 				splitcord= move.split(',')
 				moveX=int(splitcord[0])
@@ -175,6 +169,7 @@ def possibleMovesR():
 				if(board[moveY][moveX]!=playeR):
 					if(moveX >=0 and moveX <8 and moveY >=0 and moveY <8):
 						if(moveY==pieceY+1 and (moveX== pieceX+1 or moveX== pieceX-1)):
+							#Updates board
 							board[moveY][moveX]=board[pieceY][pieceX]
 							board[pieceY][pieceX]=0
 							printGrid()
@@ -184,24 +179,24 @@ def possibleMovesR():
 
 				else:
 					print('A piece is already there!')
+			else:
+				possibleMovesR()
 	except:
 		pass
+#Another jump will be taken if there's an enemy's piece to be eaten
 def doubleJumpR():
 	while((board[newPosY+1][newPosX+1]==playerB or board[newPosY+1][newPosX-1]==playerB) and (board[newPosY+2][newPosX+2]==0 or board[newPosY+2][newPosX-2]==0)):
 		if((board[newPosY+1][newPosX-1]==playerB) ):
 			input('Another available piece to be eaten. Press enter to continue')
+			# Updates board values
 			board[newPosY+2][newPosX-2]=board[newPosY][newPosX]
 			board[newPosY][newPosX]=0
 			board[newPosY+1][newPosX-1]=0
-			#newPosY=newPosY+2
-			#newPosX=newPosX-2
 		elif((board[newPosY+1][newPosX+1]==playerB) ):
 			input('Another available piece to be eaten. Press enter to continue')
 			board[newPosY+2][newPosX+2]=board[newPosY][newPosX]
 			board[newPosY][newPosX]=0
 			board[newPosY+1][newPosX+1]=0
-			#newPosY=newPosY+2
-			#newPosX=newPosX+2
 
 def validateAI():
 	pieceY=(random.randint(0,7))
@@ -237,20 +232,20 @@ def validateAI():
 					turn=0
 			if(((board[pieceY-1][pieceX-1]==0 or board[pieceY-1][pieceX+1]==0) and turn==1) and pieceX >=0 and (pieceX <8 and pieceY >=0 and pieceY <8)):
 				print('\n''Your enemy has chosen: '+str(pieceX)+', '+str(pieceY)+'\n')
-				if(board[pieceY-1][pieceX-1]==0):
+				if(board[pieceY-1][pieceX-1]==0 and turn!=0):
 					board[pieceY-1][pieceX-1]=board[pieceY][pieceX]
 					board[pieceY][pieceX]=0
 					printGrid()
-					print('\n''And moved it to '+str(newPosX)+', '+str(newPosY)+'\n')
+					print('\n''And moved it to '+str(pieceX-1)+', '+str(pieceY-1)+'\n')
 					turn=0
-				if(board[pieceY-1][pieceX+1]==0):
+				if(board[pieceY-1][pieceX+1]==0 and turn!=0):
 					board[pieceY-1][pieceX+1]=board[pieceY][pieceX]
 					board[pieceY][pieceX]=0
 					printGrid()
-					print('\n''And moved it to '+str(newPosX)+', '+str(newPosY)+'\n')
+					print('\n''And moved it to '+str(pieceX+1)+', '+str(pieceY-1)+'\n')
 					turn=0
 def validateB():
-		global piece, pieceX, pieceY, move, moveX, moveY, board,turn
+		global piece, pieceX, pieceY, move, moveX, moveY, board,turn,board2,copyBoard
 		piece = input('\n' 'Its player B turn! Choose your piece to move or EXIT to quit:' '\n')
 		if ( piece == 'help'):
 		    print('Type the coordinates (originating from the top left) of the box you want to put a cross into in the format \'y,x\' (e.g. 3,2) or EXIT to quit''\n')
@@ -263,30 +258,18 @@ def validateB():
 			if(pieceX >=0 and pieceX <8 and pieceY >=0 and pieceY <8):
 				if(board[pieceY][pieceX]== playerB):
 					print('\n''Right piece!''\n')
-					copyBoard=board[:]
+					copyBoard= board[:]
 					copyBoard= copy.deepcopy(board)
+					board2=board[:]
 					possibleMovesB()
-					undoBoard=input ('\n''Type undo if you would like to retake the turn. Otherwise press enter''\n')
-					if(undoBoard=='undo' or undoBoard=='Undo'):
-						board=copyBoard[:]
-						board=copy.deepcopy(copyBoard)
-						printGrid()
-						
-						redoBoard=input('\n''Type redo if you would like to retake the turn. Otherwise press enter''\n')
-						if(redoBoard=='redo' or redoBoard=='Redo'):
-							copyBoard=board[:]
-							copyBoard=copy.deepcopy(board)
-							printGrid()
-							turn=0
-						else:
-							turn=0
+					undoRedo()
 				else:
 					print('Cannot move this piece. Try again :)')
 				
 		else:
 			print('Invalid input. Type \'help\' if you\'re stuck')
 def possibleMovesB():
-	global newPosX, newPosY
+	global newPosX, newPosY,turn
 	try:
 		if((board[pieceY-1][pieceX-1]==playeR or board[pieceY-1][pieceX+1]==playeR) and (board[pieceY-2][pieceX+2]==0 or board[pieceY-2][pieceX-2]==0)):
 			eat=input('\n' 'Available piece to be eaten.Press Y to eat or N not to' '\n')					
@@ -299,7 +282,6 @@ def possibleMovesB():
 					newPosX=pieceX-2
 					printGrid()
 					print('\n' 'CONGRATS! You ate an enemy piece!' '\n')
-					global turn
 					doubleJumpB()
 					printGrid()
 					turn=0
@@ -331,6 +313,8 @@ def possibleMovesB():
 								print('This move is not allowed')
 					else:
 						print('A piece is already there!')
+			else:
+				possibleMovesB()
 		elif((board[pieceY-1][pieceX-1]==playeR or board[pieceY-1][pieceX+1]==playeR) and(board[pieceY+2][pieceX+2]!=0 or board[pieceY+2][pieceX-2]!=0)):
 			print('...but there is not legal moves available. Try again :D')
 		
@@ -355,6 +339,8 @@ def possibleMovesB():
 							print('This move is not allowed')
 				else:
 					print('A piece is already there!')
+			else:
+				possibleMovesB()
 	except:
 		pass	
 
@@ -375,6 +361,26 @@ def doubleJumpB():
 			board[newPosY-1][newPosX+1]=0
 			#newPosY=newPosY-2
 			#newPosX=newPosX+2
+def undoRedo():
+	global board, copyBoard,board2, turn
+	undoBoard=input ('\n''Type undo if you would like to retake the turn. Otherwise press enter''\n')
+	if(undoBoard=='undo' or undoBoard=='Undo'):
+		board=copyBoard[:]
+		board=copy.deepcopy(copyBoard)
+		printGrid()
+		redoBoard=input('\n''Type redo if you would like to retake the turn. Otherwise press enter''\n')
+		if(redoBoard=='redo' or redoBoard=='Redo'):
+			board=board2[:]
+			board=copy.deepcopy(board2)
+			printGrid()
+			if(turn==1):
+				turn=1
+			elif(turn==0):
+				turn=0
+		elif(redoBoard!='redo' and turn==1):
+			validateR()
+		elif(redoBoard!='redo' and turn==0):
+			validateB()
 def CheckKing():
 	for i in range(8):
 		if(board[7][i]==1):
@@ -407,6 +413,8 @@ def welcomeStart():
 				print('Incorrect input')
 	if(gameMode=='exit'):
 		sys.exit()
+	else:
+		welcomeStart()
 
 #starts the game
 welcomeStart()
